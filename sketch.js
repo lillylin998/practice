5658482037d8d04f988b0fc92856542cf8ca3e61
Lilly Lin <lillylin998@users.noreply.github.com>
2020-10-23 10:06:24 -0700

    update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,31 @@
+let corgiImg;
+
+function preload(){
+    url = 'https://dog.ceo/api/breed/corgi/images';
+    data = loadJSON(url);
+ //   print(data);
+ //   display(data,10);
+} 
+
 function setup() {
   createCanvas(400, 400);
 }
-
+   // display(data,10);
 function draw() {
   background(220);
 
-  fill(0);
-  ellipse(200,200,50,50);
-  
+  display(data,10);
+  image(corgiImg, 200,200);
+}
+
+function display(data,i){
+  if (data.status === 'success'){
+    let corgi = data.message[i];
+     // print(corgi);
+    corgiImg = loadImage(corgi);
+     // print(corgiImg);
+    
+  } else{
+    print('failed');
+  }
 }
